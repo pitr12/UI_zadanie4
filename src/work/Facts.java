9a0829fadc79f88b4dca6c183478f8933beb89e2
Peter Dubec <peter.dubec93@gmail.com>
2014-05-10 18:15:29 +0200

    Initialize git repository

---
+++ src/work/Facts.java
@@ -0,0 +1,17 @@
+package work;
+
+public class Facts {
+	private String fact;
+	
+	/**
+	 * constructor*/
+	public Facts(String fact){
+		this.fact = fact;
+	}
+	
+	/**
+	 * get Fact*/
+	public String getFact(){
+		return this.fact;
+	}
+}
